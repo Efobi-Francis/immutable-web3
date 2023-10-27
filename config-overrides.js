1f92d776b239fb0e1fe802048c586e9b50e0769a
f0b <francisefobi@gmail.com>
2023-10-28 00:17:14 +0100

    update

--- config-overrides.js
+++ config-overrides.js
@@ -15,7 +15,8 @@ module.exports = function override(config, env) {
       "stream": require.resolve('stream-browserify'),
       "crypto": require.resolve('crypto-browserify'),
       "assert": require.resolve("assert"),
-      "path": require.resolve("path-browserify")
+      "path": require.resolve("path-browserify"),
+      "os": require.resolve("os-browserify"),
     },
   };
   
