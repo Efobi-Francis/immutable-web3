6960c934ee5858d9819fc6d81ff47aba0257e297
f0b <francisefobi@gmail.com>
2023-10-28 03:20:55 +0100

    Update PrivateRoute.jsx

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,18 +1,9 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from './AuthContext.jsx';
 
-export const PrivateRoute = ( {children} ) => {
+export const PrivateRoute = ({ children }) => {
   const { isAuthenticated } = useContext(AuthContext);
 
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      return children
-    } else {
-        return navigate('/login');
-    }
-  }, [isAuthenticated, navigate]);
+  return !isAuthenticated ? children : <Navigate to="/login" replace />;
 };
